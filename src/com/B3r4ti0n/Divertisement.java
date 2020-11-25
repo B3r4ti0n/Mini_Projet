f692eed3496d2cce0d07b644d9b20f53304bd30c
scappaticci <lorenzoscappaticci12@gmail.com>
2020-11-25 13:38:41 +0100

    Class

---
+++ src/com/B3r4ti0n/Divertisement.java
@@ -0,0 +1,12 @@
+package com.B3r4ti0n;
+
+public class Divertisement {
+
+    public static void Jeu() {
+
+    }
+
+    public static void musique() {
+
+    }
+}
---
+++ src/com/B3r4ti0n/Gestion_administrative.java
@@ -0,0 +1,13 @@
+package com.B3r4ti0n;
+import java.util.Scanner;
+
+public class Gestion_administrative {
+
+    public static void gestion_rdv() {
+        System.out.println("1 pour voir les rdv, 2 pour ajouter un rdv ");
+    }
+
+    public static void gestion_reservation_chambre() {
+
+    }
+}
