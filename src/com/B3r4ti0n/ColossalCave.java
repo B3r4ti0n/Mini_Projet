6db75dc8be54501476f27020d739ad1d96527ebe
Océane <oceanekegoff@gmail.com>
2020-11-26 16:15:03 +0100

    Jeu finit

--- src/com/B3r4ti0n/ColossalCave.java
+++ src/com/B3r4ti0n/ColossalCave.java
@@ -1,7 +1,5 @@
 package com.B3r4ti0n;
 
-import com.B3r4ti0n.Jeu.Chemin;
-
 import java.util.Scanner;
 
 public class ColossalCave {
@@ -9,7 +7,9 @@ public class ColossalCave {
     public static void regle() {
         System.out.println("Bienvenu sur Colossal Cave\n"+ "\n"+
                 "Ce jeu d’aventure est purement textuel. Son fonctionnement est simple :\n"+
-                "Un paragraphe décrit le lieu où l'on se trouve, et l'on tape des instructions au clavier pour se déplacer ou résoudre des énigmes diverses.\n");
+                "Un paragraphe décrit le lieu où l'on se trouve, et l'on tape des instructions au clavier pour se déplacer ou résoudre des énigmes diverses.\n"+
+                "Vous choisissez le premier lieu en fonction de son nombre. \n"+
+                "Pour se déplacer il faut choisir N, S, O, E ou encore NO, NE.\n");
 
         String ChoixMenu;
         System.out.println("Pour Jouer tapez '1'");
@@ -32,56 +32,128 @@ public class ColossalCave {
         int Foret = 5;
         Scanner scanner = new Scanner(System.in);
 
-        Chemin buildingRoute = new Chemin(0, 0, 0, Route);
-        Chemin foretRoute = new Chemin(0, Route, 0, 0);
-        Chemin collineForet = new Chemin(Foret, 0, Foret, 0);
-        Chemin valleeRoute = new Chemin(Route, 0, 0, 0);
-        Chemin valleeColline = new Chemin(Colline, 0, 0, Colline);
-        Chemin routeColline = new Chemin(0, 0, 0, Colline);
-        Chemin routeVallee = new Chemin(0, Vallee, 0, 0);
-        Chemin routeBuilding = new Chemin(0, 0, Building, 0);
-        Chemin routeForet = new Chemin(Foret, 0, 0, 0);
-
-        // 0 DEPART  //  N, S, E, W
-        // 1 ROUTE -> 2, 3, 4, 5 / W, E, S, N
-        // 2 COLLINE -> 5 / N&E
-        // 3 BUILDING EN BRIQUE -> 1 / W
-        // 4 VALLÉE -> 1, 2 / N, O&N
-        // 5 FORET -> 1 / S
-
         System.out.println("Bienvenu sur Colossal Cave" +"\n"+
                 "Veuillez choisir votre point de départ :" +"\n"+
                 "1/ Route" +"\n"+ "2/ Colline" +"\n"+ "3/ Building en brique" +"\n"+ "4/ Vallée " +"\n"+ "5/ Forêt");
         int depart = scanner.nextInt();
-        if (depart == Route){
-            //com.B3r4ti0n.Jeu.Route.run();
+        if (depart == Route || depart == Colline || depart == Building || depart == Vallee || depart == Foret) {
             mouvement(depart);
-        } else if (depart == Colline) {
-            com.B3r4ti0n.Jeu.Colline.run();
-        } else if (depart == Building) {
-            com.B3r4ti0n.Jeu.Building.run();
-        } else if (depart == Vallee) {
-            com.B3r4ti0n.Jeu.Vallee.run();
-        } else if (depart == Foret) {
-            com.B3r4ti0n.Jeu.Foret.run();
         }
     }
+
     public static int mouvement(int depart){
-        if(depart == 1){
-            System.out.println("rentre de chemin");
+        if (depart == 1) {  //Départ de la route
+            System.out.println(" Vous êtes au bout de la route devant un petit immeuble en brique.\n"+
+                    "Un petit ruisseau coule a côté du building en bas d’une rigole. \n");
+            System.out.println("Choisit le prochain lieu :" +"\n"+
+                    "A l'Ouest se trouve la Colline 'O' \n"+
+                    "A l'Est se trouve le Building en brique 'E' \n"+
+                    "Au Sud se trouve la Vallée 'S' \n"+
+                    "Au Nord se trouve la Forêt 'N' ");
            String event = Function.scan();
             switch(event){
-                case "2":
-                    System.out.println(" Vous êtes au bout de la route devant un petit immeuble en brique.\n"+
-                            "Un petit ruisseau coule a côté du building en bas d’une rigole.");
+                case "O":
+                    System.out.println("---------- Vous avancez vers l'Ouest ---------- \n");
+                    System.out.println(" Vous êtes tout en haut d’une colline qui surplombe la ville,\n"+
+                            "au loin vous voyez une ombre se déplacer a grande vitesse avec un grand couteau. \n");
                     break;
-                case "3":
-                    System.out.println("");
+                case "E":
+                    System.out.println("---------- Vous avancez vers l'Est ---------- \n");
+                    System.out.println(" Vous êtes a l’intérieur d’un building en brique, un petit refuge pour les bêtes.\n"+
+                            "Un homme git là au milieu d’une marre de sang. \n");
+                    break;
+                case "S":
+                    System.out.println("---------- Vous avancez vers le Sud ---------- \n");
+                    System.out.println("Vous êtes dans une vallée a coté d’un ruisseau.\n"+
+                            "Un corps flotte là dans l’eau. \n");
+                    break;
+                case "N":
+                    System.out.println("---------- Vous avancez vers le Nord ---------- \n");
+                    System.out.println("Vous êtes dans la foret noire.\n"+
+                            "Vous entendez des hurlements stridents. \n");
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + event);
             }
             return 1;
+        }
+
+        if (depart == 2) {    //Départ de la colline
+            System.out.println(" Vous êtes tout en haut d’une colline qui surplombe la ville,\n"+
+                    "au loin vous voyez une ombre se déplacer a grande vitesse avec un grand couteau. \n");
+            System.out.println("Choisit le prochain lieu :" +"\n"+
+                    "Au Nord-Est se trouve la Forêt 'NO' ");
+            String event = Function.scan();
+            switch(event){
+                case "NO":
+                    System.out.println("---------- Vous avancez vers le Nord-Est ---------- \n");
+                    System.out.println("Vous êtes dans la foret noire.\n"+
+                            "Vous entendez des hurlements stridents. \n");
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + event);
+            }
+            return 2;
+        }
+
+        if (depart == 3) {  //Départ du building
+            System.out.println(" Vous êtes a l’intérieur d’un building en brique, un petit refuge pour les bêtes.\n"+
+                    "Un homme git là au milieu d’une marre de sang. \n");
+            System.out.println("Choisit le prochain lieu :" +"\n"+
+                    "A l'Ouest se trouve la Route 'O' ");
+            String event = Function.scan();
+            switch(event){
+                case "O":
+                    System.out.println("---------- Vous avancez vers l'Ouest ---------- \n");
+                    System.out.println(" Vous êtes au bout de la route devant un petit immeuble en brique.\n"+
+                            "Un petit ruisseau coule a côté du building en bas d’une rigole. \n");
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + event);
+            }
+            return 3;
+        }
+
+        if (depart == 4) {  //Départ de la Vallée
+            System.out.println("Vous êtes dans une vallée a coté d’un ruisseau.\n"+
+                    "Un corps flotte là dans l’eau. \n");
+            System.out.println("Choisit le prochain lieu :" +"\n"+
+                    "Au Nord se trouve la Route 'N' \n"+
+                    "Au Nord-Ouest se trouve la Colline 'NO' ");
+            String event = Function.scan();
+            switch(event){
+                case "N":
+                    System.out.println("---------- Vous avancez vers le Nord ---------- \n");
+                    System.out.println(" Vous êtes au bout de la route devant un petit immeuble en brique.\n"+
+                            "Un petit ruisseau coule a côté du building en bas d’une rigole. \n");
+                    break;
+                case "NO":
+                    System.out.println("---------- Vous avancez vers le Nord-Ouest ---------- \n");
+                    System.out.println(" Vous êtes tout en haut d’une colline qui surplombe la ville,\n"+
+                            "au loin vous voyez une ombre se déplacer a grande vitesse avec un grand couteau. \n");
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + event);
+            }
+            return 4;
+        }
+
+        if (depart == 5) {  //Départ de la Forêt
+            System.out.println("Vous êtes dans la foret noire.\n"+
+                    "Vous entendez des hurlements stridents. \n");
+            System.out.println("Choisit le prochain lieu :" +"\n"+
+                    "Au Sud se trouve la Route 'S' ");
+            String event = Function.scan();
+            switch(event){
+                case "S":
+                    System.out.println("---------- Vous avancez vers le Sud ---------- \n");
+                    System.out.println(" Vous êtes au bout de la route devant un petit immeuble en brique.\n"+
+                            "Un petit ruisseau coule a côté du building en bas d’une rigole. \n");
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + event);
+            }
+            return 5;
         }
         return 0;
     }
--- src/com/B3r4ti0n/Jeu/Building.java
+++
@@ -1,9 +0,0 @@
-package com.B3r4ti0n.Jeu;
-
-public class Building {
-
-    public static void run() {
-        System.out.println(" Vous êtes a l’intérieur d’un building en brique, un petit refuge pour les bêtes.\n"+
-                        "Un homme git là au milieu d’une marre de sang.");
-    }
-}
